4b60d1ec22763f00e0f7abb8d64f2c48241af415
John B <jbonczek1@ivytech.edu>
2022-07-07 23:22:21 -0500

    Simple route created for home and courses

--- final-project/src/App.vue
+++ final-project/src/App.vue
@@ -1,38 +1,54 @@
-<template>
-    <v-toolbar dense>
-      <v-btn>Home</v-btn>
-      <v-btn>Courses</v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <MainHome/>
-    </v-content>
-</template>
-
 <script>
+
 import MainHome from './components/MainHome.vue'
-import Courses from './components/Courses.vue'
+import MainCourses from './components/MainCourses.vue'
+
+const routes = {
+  '/': MainHome,
+  '/Courses': MainCourses
+}
 
 export default {
   name: 'App',
 
   components: {
     MainHome,
-    Courses
+    MainCourses
   },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/']
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+    })
+  }
 }
+
 </script>
+
+<template>
+  <v-toolbar dense>
+    <v-btn href="#/">Home</v-btn>
+    <v-btn href="#/Courses">Courses</v-btn>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+    
+  </v-toolbar>
+  <component :is="currentView"/>
+</template>
